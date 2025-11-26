30e3ec9fbc26c469a4985ed19b1942c870697bf3
aridspace10 <jackovand27@icloud.com>
2025-11-27 01:48:56 +1000

    Added add_directory

--- filesystem.py
+++ filesystem.py
@@ -1,13 +1,29 @@
+from filenode import FileNode
+
 
 class FileSystem:
     def __init__(self):
-        self.items = []
+        self.filehead = FileNode(None, "..")
+        self.current: FileNode = self.filehead
 
     def setup_system(self, textfile):
         pass
 
-    def add_directory(self, path):
-        pass
+    def add_directory(self, path: str) -> str:
+        self.inital = self.current
+        while (lst := path.split("/")):
+            if (self.current.search(lst[0])) in [None, "file"]:
+                temp = self.current
+                self.current = self.inital
+                return f"No directory named {lst[0]} in \"${temp}\""
+            node = self.current.access(lst[0])
+            if node is None:
+                temp = self.current
+                self.current = self.inital
+                return f"No directory named {lst[0]} in \"${temp}\""
+            self.current = node
+        self.current = self.current.add_child(path, 'directory')
+        return ""
 
     def add_file(self, path):
         pass
