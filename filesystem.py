77d0983458a9afe6010bfe9d56f1337c5a255263
aridspace10 <jackovand27@icloud.com>
2025-11-27 01:10:49 +1000

    setup filesystem file

---
+++ filesystem.py
@@ -0,0 +1,13 @@
+
+class FileSystem:
+    def __init__(self):
+        self.items = []
+
+    def setup_system(self, textfile):
+        pass
+
+    def add_directory(self, path):
+        pass
+
+    def add_file(self, path):
+        pass
