5076d369214c438feafede081051c4ed91bbe811
aridspace10 <jackovand27@icloud.com>
2025-11-27 21:22:49 +1000

    worked on search, list files and setup system

--- filesystem.py
+++ filesystem.py
@@ -7,21 +7,40 @@ class FileSystem:
         self.current: FileNode = self.filehead
 
     def setup_system(self, textfile):
-        pass
+        temp = self.current
+        self.current = self.filehead
+        with open(textfile) as f:
+            for line in f:
+                if (self.add_directory(line)):
+                    raise Exception("Adminstator Error, Code AAA123")
+        self.current = temp
 
-    def add_directory(self, path: str) -> str:
-        self.inital = self.current
-        while (lst := path.split("/")):
+    def list_files(self, path):
+        if (path != ""):
+            if (error := self.search(path)) != "":
+                return error
+        return self.current.list_content()
+
+    def search(self, path: str) -> str:
+        while (len(lst := path.split("/")) > 1):
+            if (lst[0] == "."):
+                continue
+            if (lst[0] == ".."):
+                self.current = self.filehead
+                continue
+
             if (self.current.search(lst[0])) in [None, "file"]:
-                temp = self.current
-                self.current = self.inital
-                return f"No directory named {lst[0]} in \"${temp}\""
+                return f"No directory named {lst[0]} in \"${self.current}\""
             node = self.current.access(lst[0])
             if node is None:
-                temp = self.current
-                self.current = self.inital
-                return f"No directory named {lst[0]} in \"${temp}\""
+                return f"No directory named {lst[0]} in \"${self.current}\""
             self.current = node
+            path = path[1]
+        return ""
+
+    def add_directory(self, path: str) -> str:
+        if (error := self.search(path)) != "":
+            return error
         self.current = self.current.add_child(path, 'directory')
         return ""
 
