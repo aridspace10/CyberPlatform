da9d3cfdae3c68386504b4682e011eb2b552886b
aridspace10 <jackovand27@icloud.com>
2025-11-29 11:21:07 +1000

    Fixed issue with filesystem and tested it

--- filesystem.py
+++ filesystem.py
@@ -1,19 +1,22 @@
 from filenode import FileNode
 
-
 class FileSystem:
     def __init__(self):
         self.filehead = FileNode(None, "..")
         self.current: FileNode = self.filehead
 
     def setup_system(self, textfile):
-        temp = self.current
-        self.current = self.filehead
         with open(textfile) as f:
             for line in f:
-                if (self.add_directory(line)):
-                    raise Exception("Adminstator Error, Code AAA123")
-        self.current = temp
+                self.current = self.filehead
+                if (err := self.add(line.strip())):
+                    print(err)
+                    raise Exception("Administrator Error, Code AAA123")
+        self.current = self.filehead
+
+    def tree(self, path: str = "."):
+        if (error := self.search(path)) != "":
+            return error
 
     def list_files(self, path):
         if (path != ""):
@@ -22,27 +25,47 @@ class FileSystem:
         return self.current.list_content()
 
     def search(self, path: str) -> str:
-        while (len(lst := path.split("/")) > 1):
-            if (lst[0] == "."):
+        lst = path.split("/")
+        while len(lst) > 1:
+            if lst[0] == ".":
+                lst.pop(0)
                 continue
-            if (lst[0] == ".."):
+            if lst[0] == "..":
                 self.current = self.filehead
+                lst.pop(0)
                 continue
 
-            if (self.current.search(lst[0])) in [None, "file"]:
-                return f"No directory named {lst[0]} in \"${self.current}\""
+            if self.current.search(lst[0]) in [None, "file"]:
+                return f"No directory named {lst[0]}"
             node = self.current.access(lst[0])
             if node is None:
-                return f"No directory named {lst[0]} in \"${self.current}\""
+                return f"No directory named {lst[0]}"
             self.current = node
-            path = path[1]
+            lst.pop(0)
         return ""
+    
+    def add(self, path: str) -> str:
+        if "." in path:
+            return self.add_file(path)
+        else:
+            return self.add_directory(path)
 
     def add_directory(self, path: str) -> str:
+        saved_current = self.current
         if (error := self.search(path)) != "":
+            self.current = saved_current
             return error
         self.current = self.current.add_child(path, 'directory')
         return ""
 
     def add_file(self, path):
-        pass
+        saved_current = self.current
+        if (error := self.search(path)) != "":
+            self.current = saved_current
+            return error
+        self.current = self.current.add_child(path, 'file')
+        return ""
+
+f = FileSystem()
+f.setup_system("filesystems/example.txt")
+print (f.list_files(""))
