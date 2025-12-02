99226ef1a3b786e1e2eb3d85faecb720d7099925
aridspace10 <jackovand27@icloud.com>
2025-12-02 14:41:38 +1000

    added start to mkdir

--- commandline.py
+++ commandline.py
@@ -8,15 +8,40 @@ class CommandLine:
     
     def enter_command(self, raw: str) -> None:
         args = raw.split(" ")
-        if (args[0] == "ls"):
-            print (self.ls(args[1:]))
+        match args[0]:
+            case "ls":
+                print (self.ls(args[1:]))
+            case "mkdir":
+                self.mkdir(args[1:])
+    
+    def mkdir(self, args: list[str]):
+        permissions = {"r": True, "w": True, "x": True}
+        while len(args) > 1:
+            arg = args[0]
+            if (arg == "-m" or arg == "--mode"):
+                args = args[1:]
+                arg = args[0]
+                if (arg.startswith("a=")):
+                    permissions = {"r": False, "w": False, "x": False}
+                    while arg:
+                        match arg[0]:
+                            case "r":
+                                permissions["r"] = True
+                            case "w":
+                                permissions["w"] = True
+                            case "x":
+                                permissions["x"] = True
+                        arg = arg[1:]
+                else:
+                    print ("mkdir: option given to -m or --mode is not correct")
+            args = args[1:]
 
     def ls(self, args: list[str]):
         deep = False
         while args:
             arg = args[0]
             if (arg[0] == "-"):
-                options = arg[1:].split("")
+                options = arg[1:].split()
                 for option in options:
                     match option:
                         case "R":
@@ -26,4 +51,4 @@ class CommandLine:
 
 cl = CommandLine()
 cl.filesystem.setup_system("filesystems/example.txt")
-cl.enter_command("ls")
+cl.enter_command("ls -R")
