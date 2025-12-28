45d9bb9d7cbe55a814e0b53a18608c0de4b33c0f
aridspace10 <jackovand27@icloud.com>
2025-12-28 14:59:20 +1000

    added data methods

--- inode.py
+++ inode.py
@@ -30,3 +30,15 @@ class Inode:
         if isinstance(self.data, bytes):
             return len(self.data)
         return len(self.data.encode("utf-8"))
+    
+    def get_data(self) -> str:
+        self.atime = datetime.datetime.now()
+        return self.data
+    
+    def set_data(self, data: str) -> None:
+        self.mtime = datetime.datetime.now()
+        self.data = data
+    
+    def append_data(self, data: str) -> None:
+        self.mtime = datetime.datetime.now()
+        self.data += "\n" + data
