92723e57cdb2c7a91ce79228cd18434a29c0ddd3
aridspace10 <jackovand27@icloud.com>
2026-02-04 21:19:31 +1000

    Added session router

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from network.WebsocketServer import router as websocket_router
+from api.sessions import router as sessions_router
 
 app = FastAPI()
 
 app.include_router(websocket_router)
+app.include_router(sessions_router)
 
 @app.get("/")
 def root():
