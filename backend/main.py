df04601b85833b4909b23919eadf47ebdd5de3c0
aridspace10 <jackovand27@icloud.com>
2026-02-04 21:19:12 +1000

    setup basic websocket

--- backend/main.py
+++ backend/main.py
@@ -1,23 +1,10 @@
 from fastapi import FastAPI
-from game.filesystem import FileSystem
-from fastapi.middleware.cors import CORSMiddleware
-from game.commandline import CommandLine
-from game.ShellState import ShellState
+from network.WebsocketServer import router as websocket_router
 
 app = FastAPI()
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
-cmd = CommandLine()
-shell = ShellState()
-shell.fs = FileSystem()
-shell.fs.setup_system("filesystems/example.txt")
-print(cmd.enter_command("cd d1 && ls", shell))
+app.include_router(websocket_router)
 
 @app.get("/")
-def health():
-    return {"status": "ok"}
+def root():
+    return {"status": "CyberPlatform backend running"}
