5738cb24db1411de32aef094d5c2327e2ccb1601
aridspace10 <jackovand27@icloud.com>
2026-02-04 21:42:54 +1000

    added middleware for api calls

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
 from network.WebsocketServer import router as websocket_router
 from api.sessions import router as sessions_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(websocket_router)
 app.include_router(sessions_router)
