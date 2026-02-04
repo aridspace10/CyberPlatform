213e7f66b60f5b3afcf2dc4015d0891df5ee9a69
aridspace10 <jackovand27@icloud.com>
2026-02-04 21:43:39 +1000

    changes to general and homepage ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Terminal from './components/Terminal'
+import Terminal from "./game/Terminal";
+import Homepage from "./Homepage";
+import "./app.css"
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <Terminal />
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+export default function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/terminal" element={<Terminal />} />
+            </Routes>
+        </Router>
+    )
 }
-
-export default App
---
+++ frontend/src/Homepage.jsx
@@ -0,0 +1,11 @@
+import SessionList from "./components/SessionsList"
+import "./Homepage.css"
+
+export default function Homepage() {
+    return (
+        <div className="homepage">
+            <h1> Hello </h1>
+            <SessionList />
+        </div>
+    )
+}
