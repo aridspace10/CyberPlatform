20c03842cc493a6ec93f7e242705053295e40b37
aridspace10 <jackovand27@icloud.com>
2026-02-05 22:00:01 +1000

    Prettifed up session list

--- frontend/src/components/SessionsList.jsx
+++ frontend/src/components/SessionsList.jsx
@@ -1,9 +1,15 @@
 
 import { useEffect, useState } from "react";
+import { Link, useNavigate } from 'react-router-dom';
+import "./SessionsList.css"
 
 export default function SessionList() {
-
+  const navigate = useNavigate();
   const [sessions, setSessions] = useState([]);
+
+  const handleEnter = async (sessionId) => {
+    navigate(`/game`, {state: {sessionId: sessionId}});
+  }
 
   useEffect(() => {
     fetch("http://localhost:8000/api/sessions")
@@ -12,14 +18,28 @@ export default function SessionList() {
   }, []);
 
   return (
-    <div>
-      <h2>Active Sessions</h2>
+  <div>
+    <h2>Active Sessions</h2>
 
-      {sessions.map(s => (
-        <div key={s.id}>
-          Session: {s.id} | Players: {s.players}
-        </div>
-      ))}
-    </div>
-  );
+    <table className="session-table">
+      <thead>
+        <tr>
+          <th>Session ID</th>
+          <th>Players</th>
+          <th></th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {sessions.map((s) => (
+          <tr key={s.id}>
+            <td>{s.id}</td>
+            <td>{s.players}</td>
+            <td><button onClick={() => handleEnter(s.id)}>Enter</button></td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  </div>
+);
 }
