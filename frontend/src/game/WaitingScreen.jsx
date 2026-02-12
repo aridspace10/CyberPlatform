7731b54385ae8750078ca45e492e773797e6d5aa
aridspace10 <jackovand27@icloud.com>
2026-02-12 22:14:19 +1000

    updated waiting screen to work with game.jsx

--- frontend/src/game/WaitingScreen.jsx
+++ frontend/src/game/WaitingScreen.jsx
@@ -1,26 +1,35 @@
 import { motion } from "motion/react"
 import { useEffect, useRef, useState } from "react";
-import { useLocation } from "react-router";
-export default function WaitingScreen() {
-    const players = ["PlayerA", "PlayerB"]
-
-    const handleStart = () => {
+import { useLocation, useNavigate } from "react-router";
+export default function WaitingScreen({players}) {
+    const location = useLocation();
+    const navigate = useNavigate();
+    const { sessionId } = location.state || {};
+    const handleStart = async () => {
+        await fetch(`http://localhost:8000/api/sessions/${sessionId}/state`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ state: "running" })
+        });
         navigate(`/game`, {state: {sessionId: sessionId}});
     }
 
     return (
         <div>
             <div className="players">
-                {players.map(player => 
+                {players.map(player => (
                     <motion.button
+                        key={player}
                         initial={{ opacity: 0, scale: 0 }}
                         animate={{ opacity: 1, scale: 1 }}
                         transition={{
                             duration: 0.4,
                             scale: { type: "spring", visualDuration: 0.4, bounce: 0.5 },
                         }}
-                    />
-                )}
+                    >
+                        {player}
+                    </motion.button>
+                ))}
             </div>
             <div className="start-btn">
                 
