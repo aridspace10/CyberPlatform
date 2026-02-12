f99a1ac381bf83b9d9a225495654c9385c38befd
aridspace10 <jackovand27@icloud.com>
2026-02-12 22:14:57 +1000

    added create session

--- frontend/src/Homepage.jsx
+++ frontend/src/Homepage.jsx
@@ -1,11 +1,20 @@
 import SessionList from "./components/SessionsList"
 import "./Homepage.css"
+import { Link, useNavigate } from 'react-router-dom';
 
 export default function Homepage() {
+    const navigate = useNavigate();
+    let nextid = 123
+    const handleSessionCreation = () => {
+        navigate(`/game`, {state: {sessionId: nextid++}});
+    }
+
     return (
         <div className="homepage">
             <h1> Hello </h1>
             <SessionList />
+            <br></br>
+            <button onClick={handleSessionCreation}> Create a new session </button>
         </div>
     )
 }
