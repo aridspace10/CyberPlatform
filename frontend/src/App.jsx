2e7283bafeeddf50151f4a160a6dae7aeebc5eee
aridspace10 <jackovand27@icloud.com>
2026-02-12 22:14:41 +1000

    reconfigured game screen router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import Terminal from "./game/Terminal";
 import Homepage from "./Homepage";
+import Game from "./game/Game";
 import "./app.css"
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
@@ -9,6 +10,7 @@ export default function App() {
             <Routes>
                 <Route path="/" element={<Homepage />} />
                 <Route path="/terminal" element={<Terminal />} />
+                <Route path="/game" element={<Game />} />
             </Routes>
         </Router>
     )
