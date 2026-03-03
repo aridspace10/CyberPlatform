8eb85878011dbc396d6dd0c6fcda2f5a9a5528d4
aridspace10 <jackovand27@icloud.com>
2026-03-03 19:12:02 +1000

    added database to main.py

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,24 @@ from fastapi import FastAPI
 from network.WebsocketServer import router as websocket_router
 from api.sessions import router as sessions_router
 from fastapi.middleware.cors import CORSMiddleware
+from contextlib import asynccontextmanager
+from .db.session import engine, Base
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # -------- STARTUP --------
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    print("Database initialized.")
+
+    yield  # ← app runs here
+
+    # -------- SHUTDOWN --------
+    await engine.dispose()
+    print("Database connection closed.")
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
