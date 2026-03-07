5a86344d4a28f6f75082cb9773f943428d3eb005
aridspace10 <jackovand27@icloud.com>
2026-03-07 13:03:23 +1000

    fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Homepage from "./Homepage";
 import Game from "./game/Game";
 import "./app.css"
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import UserManager from "./components/UserManager";
 
 export default function App() {
     return (
@@ -11,6 +12,7 @@ export default function App() {
                 <Route path="/" element={<Homepage />} />
                 <Route path="/terminal" element={<Terminal />} />
                 <Route path="/game" element={<Game />} />
+                <Route path="/users" element={<UserManager />} />
             </Routes>
         </Router>
     )
