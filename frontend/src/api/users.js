8fd5d236a5218b244437c3a36fcd29fab159db0b
aridspace10 <jackovand27@icloud.com>
2026-03-07 12:37:28 +1000

    added frontend query ability

---
+++ frontend/src/api/users.js
@@ -0,0 +1,32 @@
+const API = "http://localhost:8000/users";
+
+export async function getUsers() {
+  const res = await fetch(API);
+  return await res.json();
+}
+
+export async function createUser(username, email) {
+  const res = await fetch(API + "/", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, email })
+  });
+
+  return await res.json();
+}
+
+export async function updateUser(id, username, email) {
+  const res = await fetch(`${API}/${id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, email })
+  });
+
+  return await res.json();
+}
+
+export async function deleteUser(id) {
+  await fetch(`${API}/${id}`, {
+    method: "DELETE"
+  });
+}
