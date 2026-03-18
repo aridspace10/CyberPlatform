34f7f13afa8cfa7d5d78fa588e661ad3ee738304
aridspace10 <jackovand27@icloud.com>
2026-03-18 17:48:49 +1000

    added auth provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,13 +7,15 @@ import UserManager from "./components/UserManager";
 
 export default function App() {
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Homepage />} />
-                <Route path="/terminal" element={<Terminal />} />
-                <Route path="/game" element={<Game />} />
-                <Route path="/users" element={<UserManager />} />
-            </Routes>
-        </Router>
+        <AuthProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Homepage />} />
+                    <Route path="/terminal" element={<Terminal />} />
+                    <Route path="/game" element={<Game />} />
+                    <Route path="/users" element={<UserManager />} />
+                </Routes>
+            </Router>
+        </AuthProvider>
     )
 }
