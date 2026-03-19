c788f911a2e3a45127a0656fecbf0aff4a974128
aridspace10 <jackovand27@icloud.com>
2026-03-19 21:40:47 +1000

    moved

---
+++ frontend/src/pages/Login/LoginPage.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { useAuth } from "../../auth/useAuth";
+
+export default function Login() {
+  const { login } = useAuth();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    await login(username, password);
+  };
+
+  return (
+    <div>
+      <input onChange={e => setUsername(e.target.value)} />
+      <input type="password" onChange={e => setPassword(e.target.value)} />
+      <button onClick={handleLogin}>Login</button>
+    </div>
+  );
+}
