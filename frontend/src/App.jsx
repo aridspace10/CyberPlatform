0c15abf813610ebe5ab258f05b707561095ea79a
aridspace10 <jackovand27@icloud.com>
2026-03-19 21:43:00 +1000

    added protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,20 @@ import Game from "./game/Game";
 import "./app.css"
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import UserManager from "./components/UserManager";
+import ProtectedRoute from "./auth/ProtectedRoute";
+import { AuthProvider } from "./auth/AuthContext";
+import Login from "./pages/Login/LoginPage";
 
 export default function App() {
     return (
         <AuthProvider>
             <Router>
                 <Routes>
+                    {/* User pathways */}
                     <Route path="/" element={<Homepage />} />
+                    <Route path="/profile" element={<ProtectedRoute></ProtectedRoute>} />
+                    <Route path="/login" element={<Login />} />
+                    {/* Dev pathways */}
                     <Route path="/terminal" element={<Terminal />} />
                     <Route path="/game" element={<Game />} />
                     <Route path="/users" element={<UserManager />} />
