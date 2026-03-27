06c2dd9af7be664aab7e4bf99567c7e09b176286
aridspace10 <jackovand27@icloud.com>
2026-03-27 23:31:05 +1000

    added add session

--- backend/services/session_service.py
+++ backend/services/session_service.py
@@ -1,8 +1,15 @@
 from sqlalchemy.orm import Session
-from db.modals import GameSession
+from db.modals import GameSession, ScenarioToSession
 
 def get_sandbox_session(db: Session, user_id: int):
     return db.query(GameSession).filter(
         GameSession.name == "Sandbox",
         GameSession.creatorID == user_id
     ).first()
+
+def add_session(db: Session, user_id: int, name: str) -> int:
+    ses = GameSession(name=name, creatorID=user_id)
+    db.add(ses)
+    db.commit()
+    db.refresh(ses)
+    return int(ses.id)
