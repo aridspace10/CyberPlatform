4f842c011fdbb3cf73c53d1f77564fdc08783454
aridspace10 <jackovand27@icloud.com>
2026-04-09 22:17:57 +1000

    addec command history

--- backend/game/ShellState.py
+++ backend/game/ShellState.py
@@ -4,6 +4,19 @@ class ShellState:
     def __init__(self):
         self.cwd = ""
         self.fs = FileSystem()
-        self.commands = []
+        self.commands = CommandHistory()
         self.vars = {}
         self.ls = 0
+
+class CommandHistory:
+    def __init__(self, maxlen=1000):
+        self.commands = []
+        self.maxlen = maxlen
+
+    def add(self, cmd: str):
+        self.commands.append(cmd)
+        if len(self.commands) > self.maxlen:
+            self.commands.pop(0)
+
+    def get(self, n: int):
+        return self.commands[n - 1]
