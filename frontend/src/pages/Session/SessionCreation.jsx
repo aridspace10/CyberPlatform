40f87a9967eda556d70fe44ee0c5b8290dc8ce64
aridspace10 <jackovand27@icloud.com>
2026-05-10 21:35:18 +1000

    working on session creatioon page

--- frontend/src/pages/Session/SessionCreation.jsx
+++ frontend/src/pages/Session/SessionCreation.jsx
@@ -1,17 +1,34 @@
 import { useEffect, useRef, useState } from "react";
 export default function SessionCreation() {
     const [name, setName] = useState("");
+    const commands = ["grep"]
+
     return (
         <div className="session-create-page">
-            <input
-                type="text"
-                placeholder="Name"
-                value={name}
-                onChange={e => setname(e.target.value)}
-                onKeyDown={handleKeyDown}
-                autoComplete="name"
-                spellCheck={false}
-            />
+            <div className="create-left-side">
+                <input
+                    type="text"
+                    placeholder="Name"
+                    value={name}
+                    onChange={e => setName(e.target.value)}
+                    autoComplete="name"
+                    spellCheck={false}
+                />
+            </div>
+            <div className="create-right-side">
+                <h1> Allowed Commands </h1>
+                <div className="command-options">
+                    <button> Select All </button>
+                    <button> Deselect All </button>
+                </div>
+                <div className="commands">
+                    {commands.map(command => 
+                        <div className="command">
+                            <h1> command </h1>
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>
     )
 }
