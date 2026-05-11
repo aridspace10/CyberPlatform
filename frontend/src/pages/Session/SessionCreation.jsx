ececb9cfaa0249c093c416154eec3e75ecc5a75c
aridspace10 <jackovand27@icloud.com>
2026-05-11 21:37:16 +1000

    working on session creation page still

--- frontend/src/pages/Session/SessionCreation.jsx
+++ frontend/src/pages/Session/SessionCreation.jsx
@@ -1,19 +1,37 @@
 import { useEffect, useRef, useState } from "react";
 export default function SessionCreation() {
     const [name, setName] = useState("");
-    const commands = ["grep"]
+    const [playType, setPlayType] = useState("");
+    const [allowPipes, setAllowPipes] = useState(false)
+
+    const typeOptions = ["SinglePlayer", "Versus"]
+    const commands = {
+        "grep": ["Case Insensitive", "Find By Filename", "Match By Line", "Match By Word", "Show Line Number"],
+        "sed":  ["Save a backup", "Case Insensitive"]
+    }
 
     return (
         <div className="session-create-page">
+            <h1> Create a new session </h1>
             <div className="create-left-side">
                 <input
                     type="text"
                     placeholder="Name"
                     value={name}
                     onChange={e => setName(e.target.value)}
-                    autoComplete="name"
                     spellCheck={false}
                 />
+                <select value={value} onChange={e => setPlayType(e.target.value)}>
+                    {typeOptions.map(opt => (
+                        <option key={opt} value={opt}>
+                        {opt}
+                        </option>
+                    ))}
+                </select>
+                <label style={{ display: "flex", alignItems: "center", gap: 8, cursor: "pointer" }}>
+                    <Switch on={allowPipes} onChange={setAllowPipes} />
+                    <span>{on ? "On" : "Off"}</span>
+                </label>
             </div>
             <div className="create-right-side">
                 <h1> Allowed Commands </h1>
@@ -24,7 +42,7 @@ export default function SessionCreation() {
                 <div className="commands">
                     {commands.map(command => 
                         <div className="command">
-                            <h1> command </h1>
+                            <h1> command </h1> 
                         </div>
                     )}
                 </div>
