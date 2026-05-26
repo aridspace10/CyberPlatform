ac27f4280a85b830a09517e698879dfe6e9b20a9
aridspace10 <jackovand27@icloud.com>
2026-05-26 21:37:38 +1000

    working on process manager

--- backend/game/ProcessManager.py
+++ backend/game/ProcessManager.py
@@ -1,3 +1,23 @@
+from enum import StrEnum
+
+class ProcessState(StrEnum):
+    RUNNING = "RUNNING"
+    STOPPED = "STOPPED"
+    TERMINATED = "TERMINATED"
+
+class Process():
+    _next_id = 1
+    def __init__(self, user: str, command: str = "", status: ProcessState = ProcessState.RUNNING):
+        self.pid: int = Process._next_id
+        Process._next_id += 1
+        self.user: str = user
+        self.command: str = command
+        self.status: str = status
+
 class ProcessManager():
     def __init__(self) -> None:
         self.processes: list[Process] = []
+
+    def create_process(self, user: str, command: str = ""):
+        process = Process(user, command)
+        self.processes.append(process)
