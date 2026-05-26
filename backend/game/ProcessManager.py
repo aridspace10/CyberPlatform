3125f584708102f5a97f7e63b8ec3190ae5b6914
aridspace10 <jackovand27@icloud.com>
2026-05-26 21:58:01 +1000

    flushing out process start before start to implemnt commands

--- backend/game/ProcessManager.py
+++ backend/game/ProcessManager.py
@@ -4,6 +4,7 @@ class ProcessState(StrEnum):
     RUNNING = "RUNNING"
     STOPPED = "STOPPED"
     TERMINATED = "TERMINATED"
+    SLEEPING = "SLEEPING"
 
 class Process():
     _next_id = 1
@@ -14,19 +15,26 @@ class Process():
         self.name: str = name
         self.command: str = command
         self.status: str = status
+    
+    def __repr__(self):
+        return f"PID={self.pid} PPID={self.ppid} {self.name}"
 
 class ProcessManager():
     def __init__(self) -> None:
         self.processes: dict[int, Process] = {}
 
-    def create_process(self, name: str, parent: int, command: str = ""):
-        process = Process(name, parent)
-        self.processes[parent] = process
+    def create_process(self, name: str, parent: int, command: str = "", status: ProcessState = ProcessState.RUNNING):
+        process = Process(name, parent, command, status)
+        self.processes[process.pid] = process
 
-    def setup_system(self):
-        self.create_process("init", 0)
-        self.create_process("networkd", 1)
-        self.create_process("sshd", 1)
-        self.create_process("journald", 1)
-        self.create_process("cron", 1)
-        self.create_process("bash", 1)
+    def get_process(self, pid: int) -> Process | None:
+        return self.processes.get(pid)
+
+    def boot(self):
+        self.create_process("init", 0,)
+        self.create_process("networkd", 1, status=ProcessState.SLEEPING)
+        self.create_process("sshd",     1, status=ProcessState.SLEEPING)
+        self.create_process("journald", 1, status=ProcessState.SLEEPING)
+        self.create_process("cron",     1, status=ProcessState.SLEEPING)
+        self.create_process("bash",     1, status=ProcessState.SLEEPING)
+        Process._next_id = 100
