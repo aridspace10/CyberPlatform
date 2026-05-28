cf270405ba32107d1664c53f0db051f0101893a8
aridspace10 <jackovand27@icloud.com>
2026-05-28 21:50:30 +1000

    still working on improving process manager

--- backend/game/ProcessManager.py
+++ backend/game/ProcessManager.py
@@ -8,27 +8,34 @@ class ProcessState(StrEnum):
 
 class Process():
     _next_id = 1
-    def __init__(self, name: str, parent: int, command: str = "", status: ProcessState = ProcessState.RUNNING):
+    def __init__(self, command: str, parent: int, status: ProcessState = ProcessState.RUNNING):
         self.pid: int = Process._next_id
         Process._next_id += 1
         self.ppid: int = parent
-        self.name: str = name
         self.command: str = command
         self.status: str = status
     
     def __repr__(self):
-        return f"PID={self.pid} PPID={self.ppid} {self.name}"
+        return f"PID={self.pid} PPID={self.ppid} {self.command}"
 
 class ProcessManager():
     def __init__(self) -> None:
         self.processes: dict[int, Process] = {}
 
-    def create_process(self, name: str, parent: int, command: str = "", status: ProcessState = ProcessState.RUNNING):
-        process = Process(name, parent, command, status)
+    def create_process(self, command: str, parent: int, status: ProcessState = ProcessState.RUNNING):
+        process = Process(command, parent, status)
         self.processes[process.pid] = process
 
     def get_process(self, pid: int) -> Process | None:
         return self.processes.get(pid)
+    
+    def remove_process(self, pid: int) -> Process | None:
+        proc = self.processes[pid]
+        del self.processes[pid]
+        return proc
+    
+    def modify_process(self, pid: int, status: ProcessState) -> None:
+        self.processes[pid].status = status
 
     def boot(self):
         self.create_process("init", 0,)
